c1d37c9a2355d788215cc771e7e7b7b5e8978de8
Ray Chung <ray.j.chung@gmail.com>
2020-06-07 19:38:52 -0700

    Add test for partialUpdate().

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,25 @@
+const partialUpdate = require("../../helpers/partialUpdate");
+
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+  it("should generate a proper partial update query with just 1 field", function() {
+    const toUpdate = {
+      username: "test",
+    };
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+    const { query, values } = partialUpdate("users", toUpdate, "id", 1);
+    expect(query).toBe("UPDATE users SET username=$1 WHERE id=$2 RETURNING *");
+    expect(values).toEqual(["test", 1]);
+  });
 
+  it("should generate a proper partial update query with 2 fields", function() {
+    const toUpdate = {
+      username: "test",
+      email: "test@test.com",
+    };
+
+    const { query, values } = partialUpdate("users", toUpdate, "id", 1);
+    expect(query).toBe("UPDATE users SET username=$1, email=$2 WHERE id=$3 RETURNING *");
+    expect(values).toEqual(["test", "test@test.com", 1]);
   });
 });
